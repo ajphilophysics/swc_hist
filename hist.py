74947195fac75ca26e46e917f1905ab8c7ac47d4
AJ Philophysics <ajphilophysics@gmail.com>
2020-02-28 10:55:26 -0500

    virgin for testing

--- hist.py
+++ hist.py
@@ -1,6 +1,8 @@
 # Randomizing Data, generating summmary statistics, and histogram
 
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 #fake data, thats normal (bell)
 
@@ -14,3 +16,5 @@ print ("Random Normal Array:", x[:30])
 print ("Mean:", np.mean(x))
 print ("Stand Dev", np.std(x))
 
+plt.hist(x)
+plt.show()
